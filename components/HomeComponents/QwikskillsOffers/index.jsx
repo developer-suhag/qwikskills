ee5b5b96aceafedd04e772508d612a4295744511
Suhag Al Amin <developersuhag@gmail.com>
2022-03-28 01:01:55 +0600

    updated content

--- components/HomeComponents/QwikskillsOffers/index.jsx
+++ components/HomeComponents/QwikskillsOffers/index.jsx
@@ -13,6 +13,7 @@ import Image from "next/image";
 import homeStyles from "../../../styles/Home.module.scss";
 import unlimitedAccess from "../../../images/unlimited-access.svg";
 import detailedExplanations from "../../../images/detailed-explanations.svg";
+import updatedContent from "../../../images/updated-content.svg";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
@@ -430,6 +431,333 @@ const QwikskillsOffers = () => {
                   sx={{ display: { xs: "none", md: "inherit" } }}
                   textColor="secondary"
                   indicatorColor="secondary"
+                  onChange={handleChange}
+                  aria-label="lab API tabs example"
+                >
+                  {/* <Image src={cloud2} alt="" /> */}
+                  <Tab
+                    className={homeStyles.tabItem}
+                    label="Creation timelines standard"
+                    value="1"
+                    sx={{ px: 5 }}
+                  />
+                  <Tab
+                    className={homeStyles.tabItem}
+                    label="Creation timelines standard"
+                    value="2"
+                    sx={{ px: 5 }}
+                  />
+                  <Tab
+                    className={homeStyles.tabItem}
+                    label="Creation timelines standard"
+                    value="3"
+                    sx={{ px: 5 }}
+                  />
+                  <Tab
+                    className={homeStyles.tabItem}
+                    label="Creation timelines standard"
+                    value="4"
+                    sx={{ px: 5 }}
+                  />
+                </TabList>
+              </Box>
+              <TabPanel value="1">
+                <Paper
+                  sx={{
+                    bgcolor: "#fff",
+                    boxShadow: "0px 11px 66px rgba(112, 174, 255, 0.12)",
+                    p: 3,
+                    borderRadius: 2,
+                  }}
+                >
+                  <Box>
+                    <Typography
+                      sx={{ fontSize: 26, fontWeight: 700 }}
+                      variant="h4"
+                    >
+                      Lorem ipsum is placeholder
+                    </Typography>
+                    <Box
+                      sx={{
+                        mt: 3,
+                        display: "grid",
+                        gridTemplateColumns: {
+                          xs: "1fr",
+                          md: " repeat(2,1fr)",
+                        },
+                        gap: 6,
+                      }}
+                    >
+                      <Box>
+                        <Typography
+                          sx={{ fontSize: 22, fontWeight: 600, mb: 2 }}
+                          variant="h5"
+                          color="secondary"
+                        >
+                          Excepteur sint
+                        </Typography>
+                        <Typography variant="body2">
+                          Lorem ipsum was purposefully designed to have no
+                          meaning, but appear like real text, making it the
+                          perfect placeholder.
+                        </Typography>
+                      </Box>
+                      <Box>
+                        <Typography
+                          sx={{ fontSize: 22, fontWeight: 600, mb: 2 }}
+                          variant="h5"
+                          color="secondary"
+                        >
+                          Excepteur sint
+                        </Typography>
+                        <Typography variant="body2">
+                          Lorem ipsum was purposefully designed to have no
+                          meaning, but appear like real text, making it the
+                          perfect placeholder.
+                        </Typography>
+                      </Box>
+                    </Box>
+                  </Box>
+                </Paper>
+              </TabPanel>
+              <TabPanel value="2">
+                <Paper
+                  sx={{
+                    bgcolor: "#fff",
+                    boxShadow: "0px 11px 66px rgba(112, 174, 255, 0.12)",
+                    p: 3,
+                    borderRadius: 2,
+                  }}
+                >
+                  <Box>
+                    <Typography
+                      sx={{ fontSize: 26, fontWeight: 700 }}
+                      variant="h4"
+                    >
+                      Lorem ipsum is placeholder - 2
+                    </Typography>
+                    <Box
+                      sx={{
+                        mt: 3,
+                        display: "grid",
+                        gridTemplateColumns: {
+                          xs: "1fr",
+                          md: " repeat(2,1fr)",
+                        },
+                        gap: 6,
+                      }}
+                    >
+                      <Box>
+                        <Typography
+                          sx={{ fontSize: 22, fontWeight: 600, mb: 2 }}
+                          variant="h5"
+                          color="secondary"
+                        >
+                          Excepteur sint
+                        </Typography>
+                        <Typography variant="body2">
+                          Lorem ipsum was purposefully designed to have no
+                          meaning, but appear like real text, making it the
+                          perfect placeholder.
+                        </Typography>
+                      </Box>
+                      <Box>
+                        <Typography
+                          sx={{ fontSize: 22, fontWeight: 600, mb: 2 }}
+                          variant="h5"
+                          color="secondary"
+                        >
+                          Excepteur sint
+                        </Typography>
+                        <Typography variant="body2">
+                          Lorem ipsum was purposefully designed to have no
+                          meaning, but appear like real text, making it the
+                          perfect placeholder.
+                        </Typography>
+                      </Box>
+                    </Box>
+                  </Box>
+                </Paper>
+              </TabPanel>
+              <TabPanel value="3">
+                <Paper
+                  sx={{
+                    bgcolor: "#fff",
+                    boxShadow: "0px 11px 66px rgba(112, 174, 255, 0.12)",
+                    p: 3,
+                    borderRadius: 2,
+                  }}
+                >
+                  <Box>
+                    <Typography
+                      sx={{ fontSize: 26, fontWeight: 700 }}
+                      variant="h4"
+                    >
+                      Lorem ipsum is placeholder - 3
+                    </Typography>
+                    <Box
+                      sx={{
+                        mt: 3,
+                        display: "grid",
+                        gridTemplateColumns: {
+                          xs: "1fr",
+                          md: " repeat(2,1fr)",
+                        },
+                        gap: 6,
+                      }}
+                    >
+                      <Box>
+                        <Typography
+                          sx={{ fontSize: 22, fontWeight: 600, mb: 2 }}
+                          variant="h5"
+                          color="secondary"
+                        >
+                          Excepteur sint
+                        </Typography>
+                        <Typography variant="body2">
+                          Lorem ipsum was purposefully designed to have no
+                          meaning, but appear like real text, making it the
+                          perfect placeholder.
+                        </Typography>
+                      </Box>
+                      <Box>
+                        <Typography
+                          sx={{ fontSize: 22, fontWeight: 600, mb: 2 }}
+                          variant="h5"
+                          color="secondary"
+                        >
+                          Excepteur sint
+                        </Typography>
+                        <Typography variant="body2">
+                          Lorem ipsum was purposefully designed to have no
+                          meaning, but appear like real text, making it the
+                          perfect placeholder.
+                        </Typography>
+                      </Box>
+                    </Box>
+                  </Box>
+                </Paper>
+              </TabPanel>
+              <TabPanel value="4">
+                <Paper
+                  sx={{
+                    bgcolor: "#fff",
+                    boxShadow: "0px 11px 66px rgba(112, 174, 255, 0.12)",
+                    p: 3,
+                    borderRadius: 2,
+                  }}
+                >
+                  <Box>
+                    <Typography
+                      sx={{ fontSize: 26, fontWeight: 700 }}
+                      variant="h4"
+                    >
+                      Lorem ipsum is placeholder - 4
+                    </Typography>
+                    <Box
+                      sx={{
+                        mt: 3,
+                        display: "grid",
+                        gridTemplateColumns: {
+                          xs: "1fr",
+                          md: " repeat(2,1fr)",
+                        },
+                        gap: 6,
+                      }}
+                    >
+                      <Box>
+                        <Typography
+                          sx={{ fontSize: 22, fontWeight: 600, mb: 2 }}
+                          variant="h5"
+                          color="secondary"
+                        >
+                          Excepteur sint
+                        </Typography>
+                        <Typography variant="body2">
+                          Lorem ipsum was purposefully designed to have no
+                          meaning, but appear like real text, making it the
+                          perfect placeholder.
+                        </Typography>
+                      </Box>
+                      <Box>
+                        <Typography
+                          sx={{ fontSize: 22, fontWeight: 600, mb: 2 }}
+                          variant="h5"
+                          color="secondary"
+                        >
+                          Excepteur sint
+                        </Typography>
+                        <Typography variant="body2">
+                          Lorem ipsum was purposefully designed to have no
+                          meaning, but appear like real text, making it the
+                          perfect placeholder.
+                        </Typography>
+                      </Box>
+                    </Box>
+                  </Box>
+                </Paper>
+              </TabPanel>
+            </TabContext>
+          </Box>
+        </Box>
+        {/* Regularly Updated Content */}
+        <Box
+          className={homeStyles.updateContentSection}
+          sx={{ flexGrow: 1, py: 2, mt: 6 }}
+        >
+          <Grid
+            container
+            spacing={{ xs: 2, md: 8 }}
+            columns={{ xs: 2, sm: 8, md: 12 }}
+            sx={{
+              alignItems: "center",
+              justifyContent: { xs: "center", md: "inherit" },
+              flexDirection: { xs: "column-reverse", md: "inherit" },
+            }}
+          >
+            <Grid item xs={2} sm={4} md={6}>
+              <Box>
+                <Typography
+                  sx={{ fontSize: { xs: 22, md: 36 }, fontWeight: 700, mb: 2 }}
+                  variant="h4"
+                >
+                  Regularly Updated Content
+                </Typography>
+                <Typography variant="body1">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam.
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={2} sm={4} md={6}>
+              <Image src={updatedContent} alt="" />
+            </Grid>
+          </Grid>
+          {/* tabs  */}
+          <Box sx={{ width: "100%", typography: "body1", my: 2 }}>
+            <TabContext value={value}>
+              <Box>
+                {/* select mode for mobile  */}
+                <Box sx={{ my: 2, display: { md: "none" } }}>
+                  <FormControl color="secondary" fullWidth>
+                    <InputLabel
+                      variant="standard"
+                      htmlFor="uncontrolled-native"
+                    >
+                      Select Option
+                    </InputLabel>
+                    <NativeSelect defaultValue={30}>
+                      <option value={1}>Creation timelines standard</option>
+                      <option value={2}>Creation timelines standard</option>
+                      <option value={3}>Creation timelines standard</option>
+                      <option value={4}>Creation timelines standard</option>
+                    </NativeSelect>
+                  </FormControl>
+                </Box>
+                {/* select mode for mobile  */}
+                <TabList
+                  sx={{ display: { xs: "none", md: "inherit" } }}
                   onChange={handleChange}
                   aria-label="lab API tabs example"
                 >
