448f1162ed81f80751d3d4b9044b13a78bd5d9bd
Suhag Al Amin <developersuhag@gmail.com>
2022-03-27 23:52:25 +0600

    tab structure

--- components/HomeComponents/QwikskillsOffers/index.jsx
+++ components/HomeComponents/QwikskillsOffers/index.jsx
@@ -1,9 +1,20 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Tabs, Typography } from "@mui/material";
 import Image from "next/image";
 import homeStyles from "../../../styles/Home.module.scss";
 import unlimitedAccess from "../../../images/unlimited-access.svg";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import Tab from "@mui/material/Tab";
+import { useState } from "react";
+import cloud2 from "../../../images/cloud-2.svg";
 
 const QwikskillsOffers = () => {
+  const [value, setValue] = useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <div className={homeStyles.QwikskillsOffersSection}>
       <Container sx={{ py: 8 }}>
@@ -50,6 +61,42 @@ const QwikskillsOffers = () => {
               <Image src={unlimitedAccess} alt="" />
             </Grid>
           </Grid>
+          {/* tabs  */}
+          <Box sx={{ width: "100%", typography: "body1" }}>
+            <TabContext value={value}>
+              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                <TabList
+                  textColor="secondary"
+                  indicatorColor="secondary"
+                  onChange={handleChange}
+                  aria-label="lab API tabs example"
+                >
+                  {/* <Image src={cloud2} alt="" /> */}
+                  <Tab
+                    className={homeStyles.tabItem}
+                    label="Item One"
+                    value="1"
+                    sx={{ mr: 5, px: 6 }}
+                  />
+                  <Tab
+                    className={homeStyles.tabItem}
+                    label="Item Two"
+                    value="2"
+                    sx={{ mr: 5, px: 6 }}
+                  />
+                  <Tab
+                    className={homeStyles.tabItem}
+                    label="Item Three"
+                    value="3"
+                    sx={{ mr: 5, px: 6 }}
+                  />
+                </TabList>
+              </Box>
+              <TabPanel value="1">Item One</TabPanel>
+              <TabPanel value="2">Item Two</TabPanel>
+              <TabPanel value="3">Item Three</TabPanel>
+            </TabContext>
+          </Box>
         </Box>
       </Container>
     </div>
